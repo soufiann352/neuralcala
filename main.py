ecb60b1cc36b42e00195e869173eccb90a058982
soufiann352 <109857587+soufiann352@users.noreply.github.com>
2022-07-23 14:31:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+import kivy
+
+kivy.require('1.9.0')
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def calc_symbol(self, symbol):
+        self.calc_field.text += symbol
+
+    def clear(self):
+        self.calc_field.text = ""
+
+    def get_result(self):
+        self.calc_field.text = str(eval(self.calc_field.text))
+
+
+class NeuralCalc(App):
+
+    def build(self):
+        return MyRoot()
+
+
+neuralcalc = NeuralCalc()
+neuralcalc.run()
+
